73a6e8cc6130bc07089ebdce9b0645b50c5cb6a2
igorpav <ipavlichenkov@gmail.com>
2018-10-31 16:57:05 +0300

    black links

--- js/modules/scrolling-navbar.js
+++ js/modules/scrolling-navbar.js
@@ -8,7 +8,9 @@
     if ($navbar.length) {
       if ($navbar.offset().top > SCROLLING_NAVBAR_OFFSET_TOP) {
         $('.scrolling-navbar').addClass('top-nav-collapse');
+
       } else {
+
         $('.scrolling-navbar').removeClass('top-nav-collapse');
       }
     }
